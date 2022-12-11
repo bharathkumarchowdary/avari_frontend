9ec2ba4fcc1e9ed6dfd27422f7bf5c610bca956c
Bharath Kumar Chowdary Gundala <bharathkumarchowdary006@gmail.com>
2022-12-11 22:21:15 +0530

    adds angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
