4598b3dd760d78e8eeb49263bb893bc9ce74c134
Bharath Kumar Chowdary Gundala <bharathkumarchowdary006@gmail.com>
2022-12-21 20:04:02 +0530

    Feature/header (#3)
    * adds styleUrls to app.component
    
    * moves header content to app-header
    creates a sticky-header directive for all sticky-header behaviour elements needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { StickyHeaderDirective } from './sticky-header.directive';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, StickyHeaderDirective, HeaderComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
   bootstrap: [AppComponent]
 })
