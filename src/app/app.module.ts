ccf35004fea3a6c2134492c334098d123fc03fea
Bharath Kumar Chowdary Gundala <bharathkumarchowdary006@gmail.com>
2022-12-27 19:15:09 +0530

    upgrades browser-list to modern web-browsers introduces an electronics image to home-page removes unused css styles of app.component.css adds button to demo overlay list functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { StickyHeaderDirective } from './sticky-header.directive';
 import { HeaderComponent } from './header/header.component';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { NgOptimizedImage } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, StickyHeaderDirective, HeaderComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule, OverlayModule, NgOptimizedImage],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
